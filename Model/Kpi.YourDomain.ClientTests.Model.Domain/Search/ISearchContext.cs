8c04333f9155de3fd06ef37e15e4956624f504ff
Kirill Voitenko <vk@conciliolabs.com>
2021-07-28 01:24:28 +0300

    Initial commit

---
+++ Model/Kpi.YourDomain.ClientTests.Model.Domain/Search/ISearchContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Kpi.YourDomain.ClientTests.Model.Domain.Search
+{
+    public interface ISearchContext
+    {
+        void Search (string value);
+
+        void SearchAndClose (string value);
+    }
+}
